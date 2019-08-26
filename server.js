5cf2c525bf4ef2c3c8c77d6b2045b20b2dc59027
mike chui <mikel.k.khui@gmail.com>
2019-08-26 16:42:49 +0000

    Updated index.html and server.js with GET routes; fixed the database tables access in psql

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+// const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -48,6 +48,15 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/:user_id", (req, res) => {
+  res.render("index");
+});
+
+app.get("/:user_id/:list", (req, res) => {
+  res.render("index");
+  console.log("Display list!");
 });
 
 app.listen(PORT, () => {

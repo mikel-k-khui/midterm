6dc7f8f5796c40b917a1f9e9a5784a517c8cb46e
mike chui <mikel.k.khui@gmail.com>
2019-08-29 17:04:01 +0000

    Completed refactoring and changing the object returning from the ParallelDots API to classify tasks into IAB taxonomy

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,56 +1,48 @@
-const axios = require('axios');
-const { apis, descs } = require('./constant');
-const WolframAlphaAPI = require('wolfram-alpha-api');
-const waApi = WolframAlphaAPI(apis["wolfram"]["params"]["appid"]);
+// load .env data into process.env
+require('dotenv').config();
+const { apiKey } = require('../../lib/db.js');
+const { apis, categories, tags } = require('./constant');
 const pd = require('paralleldots');
-pd.apiKey = apis["paralleldots"]["apiKey"];
+pd.apiKey = apiKey;
+// const axios = require('axios');
+
 /**
  * Parse out JSON files and return it in more readable format for ouput
 */
 const parseJSON = (requestBody) => JSON.parse(requestBody);
-// let promises =[];
-// let values = [];
-// let query = '' || process.argv[2];
 
-// const getTaxonomies = function(tasks) {
-//   pd.taxonomyBatch(JSON.stringify(tasks))
-//     .then((res) => {
-//       const response = parseJSON(res)["taxonomy"];
-//       for (const res in response) {
-//         console.log(descs[res], ",", response[res][0]["tag"], ",", response[res][1]["tag"], ",", response[res][2]["tag"]);
-//       }
-//     }).catch((e) => e)
-// };
-// getTaxonomies(descs);
+let query = '' || process.argv[2];
 
 const getTaxonomy = function(task) {
+  if (task.length !== 0) {
   return pd.taxonomy(task)
-    .then(res => {
-      const response = parseJSON(res)["taxonomy"];
-      console.log(response);
-      return response;
+    .then(response => {
+      const res = parseJSON(response)['taxonomy'][0];
+      (tags[res['tag']] !== undefined) ? res['category'] = tags[res['tag']] : res['category'] = categories[Math.floor(Math.random() * categories.length)];
+      console.log(res);
+      return res;
     })
     .catch(e => console.log(e));
+  } else {
+    return null;
+  }
 };
-// getTaxonomy(query);
-
-// const getDatatypes = function() {
-
-//   for (const desc of descs) {
-//     promises.push(waApi.getFull(desc).then(res => res.datatypes));
-//   }
-
-//   Promise.all(promises)
-//     .then(values => console.log(values));
-// };
-// getDatatypes();
-
-// waApi.getSimple(apis["wolfram"]["config"]["params"]["input"]).then(res => console.log("Simple", res)).catch(console.error);
+getTaxonomy(query);
 
 module.exports = { getTaxonomy , };
 
 // /*
 // $ curl 'https://api.ipify.org?format=json'
-// {"ip":"184.68.214.222"}
+// {'ip':'184.68.214.222'}
 // source: https://www.ipify.org/
 // */
+// const getTaxonomies = function(tasks) {
+//   pd.taxonomyBatch(JSON.stringify(tasks))
+//     .then((res) => {
+//       const response = parseJSON(res)['taxonomy'];
+//       for (const res in response) {
+//         console.log(descs[res], ',', response[res][0]['tag'], ',', response[res][1]['tag'], ',', response[res][2]['tag']);
+//       }
+//     }).catch((e) => e)
+// };
+// getTaxonomies(descs);
--- public/scripts/constant.js
+++ public/scripts/constant.js
@@ -1,17 +1,29 @@
 module.exports = {
-  apis: {
-    wolfram: {
-      params: {
-        input: '',
-        appid: '5G9XE5-23E96PT5RV' },
-    },
-    paralleldots: {
-      apiKey: 'tSau7smFUeQqc1rY7wChiMIDcIQEdg3dfaq31zrvzUg'
-    },
-    yelp: {},
-    google: {}
+  tags: {
+    'ARTS & CULTURE': 'watch',
+    'BUSINESS': 'buy',
+    'CRIME': 'buy',
+    'EDUCATION': 'read',
+    'ENTERTAINMENT': 'watch',
+    'GREEN': 'buy',
+    'HEALTHY LIVING': 'eat',
+    'IMPACT': 'buy',
+    'POLITICS': 'read',
+    'SCIENCE': 'read',
+    'SPORTS': 'watch',
+    'TASTE': 'eat',
+    'TECH': 'buy',
+    'TRAVEL': 'buy',
+    'WORLDPOST': 'read'
   },
-  descs: ['Action figures', 'apple', 'candy', 'carrots', 'cheese', 'chinese', 'Chinese buffet at chinatown', 'chocolate', 'Chocolate', 'cookies', 'eat candy', 'Harry Potter', 'Harry Potter Restaurant', 'Harry Potter theme park', 'Harry Potter theme park tickets', 'Harry Potter Theme Park Tickets', 'Hobbit', 'Hobbit', 'Home made smoked meat', 'How to get away with murder', 'Hunger Games', 'Lightsaber', 'my shborts', 'orange', 'Poems by the artist at the poetry last thursday', 'Present for mom', 'salad', 'Star Wars Episode III', 'Star Wars Episode III', 'Star Wars themed cookies', 'Stranger Things', 'sushi', 'tacos', 'Tacos at Extreme Tacos', 'The Hobbit']
+  categories: ['eat', 'buy', 'read', 'watch'],
+  categoryInfo: {
+    eat: { title: 'To Eat'},
+    buy: { title: 'To Buy'},
+    read: { title: 'To Read'},
+    watch: { title: 'To Watch'}
+  },
+  descs: ['Action figures', 'apple', 'candy', 'carrots', 'cheese', 'chinese', 'Chinese buffet at chinatown', 'chocolate', 'Chocolate', 'cookies', 'eat candy', 'Harry Potter', 'Harry Potter Restaurant', 'Harry Potter theme park', 'Harry Potter theme park tickets', 'Harry Potter Theme Park Tickets', 'Hobbit', 'Hobbit', 'Home made smoked meat', 'How to get away with murder', 'Hunger Games', 'Lightsaber', 'my shorts', 'orange', 'Poems by the artist at the poetry last thursday', 'Present for mom', 'salad', 'Star Wars Episode III', 'Star Wars Episode III', 'Star Wars themed cookies', 'Stranger Things', 'sushi', 'tacos', 'Tacos at Extreme Tacos', 'The Hobbit']
 };
 // headers: { 'x-api-key': 'DEMO-API-KEY' }
 // headers: {'User-Agent': 'request'}
